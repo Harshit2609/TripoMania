103fb8825f8b0c835b55320197271e5842a5de45
Harshit Joshi <harshitjoshi2022@gmail.com>
2018-05-10 00:42:33 -0700

    Add files via upload

---
+++ Java/delhiculture.java
@@ -0,0 +1,13 @@
+package com.example.harshit.secondproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class delhiculture extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delhiculture);
+    }
+}
